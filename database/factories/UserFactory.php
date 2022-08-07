3b0b308d8721f3a437589814fcdfa1eb62e08d84
Artur Alves <artur.alves@stafiz.com>
2022-08-07 19:47:03 +0100

    Fix failing test_other_browser_sessions_can_be_logged_out

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Team;
 use App\Models\User;
+use App\Models\UserType;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use Laravel\Jetstream\Features;
@@ -30,6 +31,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'user_type_id' => UserType::TYPE_USER_ID,
         ];
     }
 
