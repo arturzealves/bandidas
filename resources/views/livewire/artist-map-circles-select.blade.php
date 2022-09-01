ec044d567a68194a50ed2db62191f0024fb7bf61
Artur Alves <artur.alves@stafiz.com>
2022-09-01 21:45:42 +0100

    Improve sidebar layout

--- resources/views/livewire/artist-map-circles-select.blade.php
+++ resources/views/livewire/artist-map-circles-select.blade.php
@@ -1,14 +1,21 @@
 <div>
-    <x-choices id="filter-student-status" wire:model.defer="selected" multiple wire:change="update">
-        @if(!empty($artists))
-            @foreach($artists as $id => $name)
-                <option value="{{ $id }}">{{ $name }}</option>
-            @endforeach
-        @endif
-    </x-select2>
-    <br>
-
-    {{ __('Budget') }}:
-    <input type="number" wire:model.lazy="budget" step="0.01" min="0" wire:change="updateBudget" value="{{ $budget }}"/>
-    <br>
+    <dl>
+        <dt>Artists</dt>
+        <dd>
+            <x-choices id="filter-student-status" wire:model.defer="selected" multiple wire:change="update">
+                @if(!empty($artists))
+                    @foreach($artists as $id => $name)
+                        <option value="{{ $id }}">{{ $name }}</option>
+                    @endforeach
+                @endif
+            </x-select2>
+        </dd>
+        <dt>Budget</dt>
+        <dd>
+            <input type="number" wire:model.lazy="budget" 
+                step="1" min="0" wire:change="updateBudget" value="{{ $budget }}"
+                class="w-full"
+            />
+        </dd>
+    </dl>
 </div>
