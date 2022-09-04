0c0069b2facf479f1cb6a003dedf8b56561ae962
Artur Alves <artur.alves@stafiz.com>
2022-09-04 19:47:42 +0100

    Finish renaming migrations and table names

--- database/migrations/2022_08_29_173943_create_map_markers_table.php
+++ database/migrations/2022_08_29_173943_create_map_markers_table.php
@@ -14,19 +14,12 @@ return new class extends Migration
      */
     public function up()
     {
-        if (!Schema::hasTable(DatabaseConstants::TABLE_GOOGLE_MAPS_USER_CIRCLES)) {
-            Schema::create(DatabaseConstants::TABLE_GOOGLE_MAPS_USER_CIRCLES, function (Blueprint $table) {
+        if (!Schema::hasTable(DatabaseConstants::TABLE_MAP_MARKERS)) {
+            Schema::create(DatabaseConstants::TABLE_MAP_MARKERS, function (Blueprint $table) {
                 $table->id();
                 $table->unsignedBigInteger('user_id');
-                $table->string('name', 100);
                 $table->decimal('latitude', 8, 6);
                 $table->decimal('longitude', 9, 6);
-                $table->mediumInteger('radius');
-                $table->char('strokeColor')->nullable();
-                $table->decimal('strokeOpacity', 3, 2)->nullable();
-                $table->tinyInteger('strokeWeight')->nullable();
-                $table->char('fillColor')->nullable();
-                $table->decimal('fillOpacity', 3, 2)->nullable();
 
                 $table->timestamps();
 
@@ -42,6 +35,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists(DatabaseConstants::TABLE_GOOGLE_MAPS_USER_CIRCLES);
+        Schema::dropIfExists(DatabaseConstants::TABLE_MAP_MARKERS);
     }
 };
