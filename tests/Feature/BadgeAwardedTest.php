4cfb16dfd445534efd565c16457e7f54c3bdd7de
Artur Alves <artur.alves@stafiz.com>
2022-09-18 21:30:56 +0100

    Add BadgeAwardedTest

---
+++ tests/Feature/BadgeAwardedTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Gamify\Events\BadgeAwarded;
+use App\Models\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Config;
+use Tests\TestCase;
+
+class BadgeAwardedTest extends TestCase
+{
+    private $event;
+    private $user;
+    private $badgeId;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->badgeId = 123;
+        $this->user = User::factory()->create();
+        $this->event = new BadgeAwarded($this->user, $this->badgeId);
+    }
+    
+    public function testbroadcastOnPrivateChannel()
+    {
+        Config::set('gamify.broadcast_on_private_channel', true);
+        
+        $this->assertInstanceOf(
+            PrivateChannel::class,
+            $this->event->broadcastOn()
+        );
+    }
+
+    public function testbroadcastOnPublicChannel()
+    {
+        Config::set('gamify.broadcast_on_private_channel', false);
+        
+        $this->assertInstanceOf(
+            Channel::class,
+            $this->event->broadcastOn()
+        );
+    }
+}
