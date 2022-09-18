aa9d48527f855e5c4b101987b8e4bf0bae5224a9
Artur Alves <artur.alves@stafiz.com>
2022-09-18 17:50:03 +0100

    Add MapCircleUpdatedListenerTest

---
+++ tests/Feature/MapCircleUpdatedListenerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Events\MapCircleUpdated;
+use App\Listeners\MapCircleUpdatedListener;
+use App\Models\MapCircle;
+use App\Models\MapMarker;
+use App\Services\GPS\LocationService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Prophecy\Argument;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Tests\TestCase;
+
+class MapCircleUpdatedListenerTest extends TestCase
+{
+    use ProphecyTrait;
+    use RefreshDatabase;
+    
+    private $listener;
+    private $locationService;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->locationService = $this->prophesize(LocationService::class);
+        $this->listener = new MapCircleUpdatedListener($this->locationService->reveal());
+    }
+    
+    public function testHandle()
+    {
+        $mapCircle = MapCircle::factory()->create();
+        $event = new MapCircleUpdated($mapCircle);
+        
+        $mapMarker = MapMarker::factory()->create([
+            'latitude' => $mapCircle->latitude + 0.0001,
+            'longitude' => $mapCircle->longitude + 0.0001,
+        ]);
+
+        $this->locationService
+            ->getDistanceBetweenLocationAndCircle(Argument::type(MapMarker::class), $event->circle)
+            ->shouldBeCalledTimes(1)
+            ->willReturn(10);
+        
+        $this->listener->handle($event);
+        
+        $this->assertContains($mapMarker->id, $event->circle->promoterMarkers->pluck('id'));
+    }
+}
