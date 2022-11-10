08e6967e143e4fb2c7562976cf5792c099775552
Artur Alves <artur.ze.alves@protonmail.com>
2022-11-10 07:59:12 +0000

    Create artist public page

--- app/Http/Controllers/ArtistController.php
+++ app/Http/Controllers/ArtistController.php
@@ -29,6 +29,7 @@ class ArtistController extends Controller
     public function show(Artist $artist)
     {
         if ($artist->spotify) {
+            $artist->largeImage = $artist->spotify->getBiggerImage();
             $artist->mediumImage = $artist->spotify->getMediumImage();
             $artist->spotify->url = json_decode($artist->spotify->external_urls)->spotify;
         }
